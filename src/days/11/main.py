f72d5418e7a4a19057ea6c92a9aaf44befaac8f6
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-10 13:41:27 -0500

    Day 11 stubs

---
+++ src/days/11/main.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import time
+
+
+def run() -> None:
+    with open("input.txt", "r", encoding="utf-8") as f:
+        commands_ = [line.rstrip() for line in f.readlines()]
+
+
+if __name__ == '__main__':
+    start = time.time()
+    run()
+    print(f"Time to execute: {time.time() - start}")
