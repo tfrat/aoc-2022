d0efe7713c113cd21bc2afc656166cf06ceb8aa4
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-13 12:59:55 -0500

    Updated the template

--- src/days/template/main.py
+++ src/days/template/main.py
@@ -3,12 +3,18 @@ from __future__ import annotations
 import time
 
 
-def run() -> None:
-    with open("input.txt", "r", encoding="utf-8") as f:
+def run(filename: str) -> None:
+    with open(filename, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f.readlines()]
+    print(f"File: {filename}")
+
+    # solution here
+
+    print()
 
 
 if __name__ == '__main__':
     start = time.time()
-    run()
+    run("example.txt")
+    run("input.txt")
     print(f"Time to execute: {time.time() - start}")
