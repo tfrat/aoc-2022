d2ddafca03d5be7079ce5f9bc502040ab3af40b3
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-14 08:57:21 -0500

    Day 14

--- src/days/14/main.py
+++ src/days/14/main.py
@@ -9,6 +9,7 @@ class Material(Enum):
     AIR = "."
     STONE = "#"
     SAND = "o"
+    VOID = "~"
 
 
 @dataclass
@@ -16,27 +17,26 @@ class Coord:
     x: int
     y: int
 
+    def move(self, direction: Coord) -> Coord:
+        return Coord(self.x + direction.x, self.y + direction.y)
+
 
 @dataclass
 class Cave:
     top_right: Coord
     bottom_left: Coord
-    horizontal_offset: int
-    sand_source: Coord
     grid: list[list[Material]]
 
-    def __init__(self, structures: list[list[Coord]], sand_source: Coord = Coord(500, 0)) -> None:
-        self.sand_source = sand_source
+    def __init__(self, structures: list[list[Coord]]) -> None:
         coords = []
         for structure in structures:
             coords.extend(structure)
         self.top_right = Coord(max(*coords, key=lambda coord: coord.x).x, 0)
         self.bottom_left = Coord(min(*coords, key=lambda coord: coord.x).x, max(*coords, key=lambda coord: coord.y).y)
-        self.horizontal_offset = self.bottom_left.x
 
         self.grid = []
-        for _ in range(self.bottom_left.y + 1):
-            self.grid.append([Material.AIR for _ in range(self.top_right.x - self.bottom_left.x + 1)])
+        for _ in range(self.bottom_left.y + 1 + 2):
+            self.grid.append([Material.AIR for _ in range(self.top_right.x * 2 + 1)])
 
         for structure in structures:
             for first, second in zip(structure, structure[1:]):
@@ -47,8 +47,17 @@ class Cave:
                     for y in range(min(first.y, second.y), max(first.y, second.y) + 1):
                         self._set_material(Coord(first.x, y), Material.STONE)
 
+        for x in range(len(self.grid[0])):
+            self._set_material(Coord(x, len(self.grid) - 1), Material.STONE)
+
     def _set_material(self, coord: Coord, material: Material) -> None:
-        self.grid[coord.y][coord.x - self.horizontal_offset] = material
+        self.grid[coord.y][coord.x] = material
+
+    def _get_material(self, coord: Coord) -> Material:
+        # Enable for VOID
+        # if not self.bottom_left.x <= coord.x <= self.top_right.x or not self.top_right.y <= coord.y <= self.bottom_left.y:
+        #     return Material.VOID
+        return self.grid[coord.y][coord.x]
 
     def __str__(self) -> str:
         out = ""
@@ -57,6 +66,25 @@ class Cave:
                 out += material.value
             out += "\n"
         return out
+
+    def drop_sand(self, source: Coord) -> bool:
+        directions = [Coord(0, 1), Coord(-1, 1), Coord(1, 1)]
+        current = source
+        can_move = True
+        while can_move:
+            can_move = False
+            for direction in directions:
+                if self._get_material(current.move(direction)) == Material.VOID:
+                    return False
+
+                if self._get_material(current.move(direction)) == Material.AIR:
+                    current = current.move(direction)
+                    can_move = True
+                    break
+        self._set_material(current, Material.SAND)
+        if current == source:
+            return False
+        return True
 
 
 def parse_structure(lines: list[str]) -> list[list[Coord]]:
@@ -74,12 +102,15 @@ def run(filename: str) -> None:
 
     coords = parse_structure(lines)
     cave = Cave(coords)
-    print(cave)
+    count = 1
+    while cave.drop_sand(Coord(500, 0)):
+        count += 1
+    print(count)
     print()
 
 
 if __name__ == '__main__':
     start = time.time()
     run("example.txt")
-    # run("input.txt")
+    run("input.txt")
     print(f"Time to execute: {time.time() - start}")
