249d8fbd365f275fa8cca8b4e8e5420d15d9cbe1
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-15 15:42:48 -0500

    Day 15 done

--- src/days/15/main.py
+++ src/days/15/main.py
@@ -2,92 +2,114 @@ from __future__ import annotations
 
 import sys
 import time
+from dataclasses import dataclass
 
 
-def taxi_distance(first: tuple[int, int], second: tuple[int, int]) -> int:
-    return abs(first[0] - second[0]) + abs(first[1] - second[1])
+@dataclass(frozen=True)
+class Coord:
+    x: int
+    y: int
+
+    def taxi_distance(self, other: Coord) -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
 
 
 class Sensor:
-    location: tuple[int, int]
-    closest_beacon: tuple[int, int]
+    location: Coord
+    closest_beacon: Coord
     distance: int
 
-    def __init__(self, location: tuple[int, int], closest_beacon: tuple[int, int]) -> None:
+    def __init__(self, location: Coord, closest_beacon: Coord) -> None:
         self.location = location
         self.closest_beacon = closest_beacon
-        self.distance = taxi_distance(location, closest_beacon)
+        self.distance = location.taxi_distance(closest_beacon)
+
+    def __str__(self) -> str:
+        return f"{self.location.x}, {self.location.y}"
 
     def __lt__(self, other: Sensor) -> bool:
-        origin = (0, 0)
-        return taxi_distance(self.location, origin) < taxi_distance(other.location, origin)
+        return self.location.x < other.location.x
 
-    def in_range(self, coord: tuple[int, int]) -> bool:
-        return taxi_distance(self.location, coord) <= self.distance
+    def get_x_range(self, y: int) -> tuple[int, int] | None:
+        if self.location.y - self.distance <= y <= self.location.y + self.distance:
+            offset = self.distance - abs(self.location.y - y)
+            return self.location.x - offset, self.location.x + offset
+        return None
 
 
-def count_no_beacons(sensors: list[Sensor], y: int, min_x: int, max_x: int) -> int:
-    hits = set()
-    sorted_sensors = sorted(sensors)
-    for x in range(min_x, max_x):
-        location = (x, y)
-        if location in hits:
+def merge_intervals(intervals: list[tuple], left_limit: int, right_limit: int) -> list[tuple]:
+    sorted_intervals = sorted(intervals, key=lambda x: x[0])
+    merged = []
+    current = None
+    for interval in sorted_intervals:
+        if current is None:
+            current = interval
             continue
-        for sensor in sorted_sensors:
-            if sensor.in_range(location) and location != sensor.closest_beacon:
-                hits.add(location)
-    return len(hits)
+        if interval[0] <= current[1]:
+            current = (max(current[0], left_limit), min(right_limit, max(current[1], interval[1])))
+        else:
+            if current[0] > right_limit:
+                break
+            merged.append(current)
+            current = None
+    if current and current[0] < right_limit:
+        merged.append(current)
+    return merged
 
 
-def find_beacon(sensors: list[Sensor], top_left: tuple[int, int], bottom_right: tuple[int,int]) -> set[tuple]:
-    hits = set()
-    for x in range(top_left[0], bottom_right[0] + 1):
-        for y in range(top_left[1], bottom_right[1] + 1):
-            location = (x, y)
-            if location in hits:
-                continue
-            hit = False
-            for sensor in sensors:
-                if sensor.in_range(location):
-                    hit = True
-                    break
-            if not hit:
-                hits.add(location)
-    return hits
+def calculate_total(intervals: list[tuple[int, int]]) -> int:
+    total = 0
+    for interval in intervals:
+        total += abs(interval[1] - interval[0]) + 1
+
+    return total
 
 
-def get_sensors(lines: list[str]) -> tuple[list[Sensor], int, int]:
+def row_intervals(sensors: list[Sensor], y: int, left_limit: int = -sys.maxsize, right_limit: int = sys.maxsize) -> list[tuple[int,int]]:
+    return merge_intervals([sensor.get_x_range(y) for sensor in sensors if sensor.get_x_range(y)], left_limit, right_limit)
+
+
+def find_beacon(sensors: list[Sensor], limit: int) -> Coord:
+    for y in range(limit + 1):
+        intervals = row_intervals(sensors, y, 0, limit)
+        if len(intervals) > 1:
+            return Coord(intervals[0][1] + 1, y)
+    raise ValueError("No Beacon found")
+
+
+def get_sensors(lines: list[str]) -> list[Sensor]:
     sensors = []
-    min_x = sys.maxsize
-    max_x = -sys.maxsize
 
     for line in lines:
         x = int(line[line.find("x=") + 2:line.find(", y")])
         y = int(line[line.find(", y=") + 4:line.find(": closest beacon")])
-        sensor_loc = (x, y)
+        sensor_loc = Coord(x, y)
         x = int(line[line.rfind("x=") + 2:line.rfind(", y")])
         y = int(line[line.rfind("y=") + 2:])
-        sensor = Sensor(sensor_loc, (x, y))
+        sensor = Sensor(sensor_loc, Coord(x, y))
         sensors.append(sensor)
-        max_x = max(sensor.location[0] + sensor.distance, max_x)
-        min_x = min(sensor.location[0] - sensor.distance, min_x)
 
-    return sensors, min_x, max_x
+    return sorted(sensors)
 
 
-def run(filename: str, y: int, part_2_bound: tuple[int, int]) -> None:
+def run(filename: str, target_y: int, limit: int) -> None:
     with open(filename, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f.readlines()]
     print(f"File: {filename}")
-    sensors, min_x, max_x = get_sensors(lines)
-    print(count_no_beacons(sensors, y, min_x, max_x))
-    # beacons = find_beacon(sensors, (0, 0), part_2_bound)
-    # print(beacons)
+    sensors = get_sensors(lines)
+    print(f"Lines covered at y=10: {calculate_total(row_intervals(sensors, target_y))}")
+
+    try:
+        beacon = find_beacon(sensors, limit)
+        print(beacon)
+        print(f"Tuning Frequency: {beacon.x * 4000000 + beacon.y}")
+    except ValueError as e:
+        print(e)
     print()
 
 
 if __name__ == '__main__':
     start = time.time()
-    run("example.txt", 10, (20, 20))
-    run("input.txt", 2000000, (4000000, 4000000))
+    run("example.txt", 10, 20)
+    run("input.txt", 2000000, 4000000)
     print(f"Time to execute: {time.time() - start}")
