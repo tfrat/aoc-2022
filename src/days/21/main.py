311b37e68ff6e9931c1ea6021bf399b311468258
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-21 13:56:41 -0500

    Day 21 part 2

--- src/days/21/main.py
+++ src/days/21/main.py
@@ -22,13 +22,40 @@ class Monkey:
         return str(self)
 
     def yell(self) -> int:
-        if not self.value:
+        if self.value is None:
             self.value = self.operation(self.left.yell(), self.right.yell())
 
         return self.value
 
 
-def parse_monkeys_part_one(lines: list[str]) -> Monkey:
+class Function:
+    root: Monkey
+    human: Monkey
+    monkeys: dict[str, Monkey]
+
+    def __init__(self, lines: list[str]) -> None:
+        self.monkeys = parse_monkeys(lines)
+        self.root = self.monkeys["root"]
+        self.human = self.monkeys["humn"]
+
+    def reset(self) -> None:
+        for monkey in self.monkeys.values():
+            if monkey.operation:
+                monkey.value = None
+
+    def lt(self, value: int) -> bool:
+        self.human.value = value
+        return self.root.left.yell() < self.root.right.yell()
+
+    def equal(self, value: int) -> bool:
+        self.human.value = value
+        return self.root.left.yell() == self.root.right.yell()
+
+    def __str__(self) -> str:
+        return f"{self.root.left.value} == {self.root.right.value}"
+
+
+def parse_monkeys(lines: list[str]) -> dict[str, Monkey]:
     monkeys = {}
     monkey_friends = {}
     for line in lines:
@@ -56,16 +83,34 @@ def parse_monkeys_part_one(lines: list[str]) -> Monkey:
         monkeys[monkey].left = monkeys[left]
         monkeys[monkey].right = monkeys[right]
 
-    return monkeys["root"]
+    return monkeys
 
+
+def find_human_number(lines: list[str]) -> int:
+    fun = Function(lines)
+    test_value = fun.human.value
+    prev_value = None
+    offset = 100000000
+    while not fun.equal(test_value):
+        difference = abs(fun.root.left.yell() - fun.root.right.yell())
+        if prev_value is not None:
+            if prev_value < difference:
+                offset *= -1
+                offset = offset // 10
+
+        prev_value = difference
+        fun.reset()
+        test_value = test_value + offset
+    return test_value
 
 
 def run(filename: str) -> None:
     with open(filename, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f.readlines()]
-    root = parse_monkeys_part_one(lines)
     print(f"File: {filename}")
-    print(root.yell())
+    root = parse_monkeys(lines)["root"]
+    print(f"Part 1: {root.yell()}")
+    print(f"Part 2: {find_human_number(lines)}")
     print()
 
     # solution here
