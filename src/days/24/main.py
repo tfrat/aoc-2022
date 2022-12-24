ac2329dced5d42b55d75b74b38656d774397b5cd
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-24 14:01:23 -0500

    Fixed w/ strings

--- src/days/24/main.py
+++ src/days/24/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import time
+from collections import deque
 from collections.abc import MutableMapping
 from dataclasses import dataclass
 from enum import Enum
@@ -8,10 +9,10 @@ from typing import Iterator
 
 
 class Direction(Enum):
-    UP = "up"
-    LEFT = "left"
-    DOWN = "down"
-    RIGHT = "right"
+    UP = "^"
+    LEFT = "<"
+    DOWN = "v"
+    RIGHT = ">"
 
     @staticmethod
     def from_char(char: str) -> Direction:
@@ -26,7 +27,6 @@ class Direction(Enum):
                 return Direction.DOWN
             case _:
                 raise ValueError(f"Not a valid direction: {char}")
-
 
 
 @dataclass(frozen=True)
@@ -98,9 +98,9 @@ class BlizzardActivity(MutableMapping[int, set[Blizzard]]):
                     case Direction.UP:
                         wrapped_location = Coord(next_blizzard.location.x, self.height - 2)
                     case Direction.RIGHT:
-                        wrapped_location = Coord(2, next_blizzard.location.y)
+                        wrapped_location = Coord(1, next_blizzard.location.y)
                     case Direction.DOWN:
-                        wrapped_location = Coord(next_blizzard.location.x, 2)
+                        wrapped_location = Coord(next_blizzard.location.x, 1)
                     case Direction.LEFT:
                         wrapped_location = Coord(self.width - 2, next_blizzard.location.y)
                     case _:
@@ -108,6 +108,9 @@ class BlizzardActivity(MutableMapping[int, set[Blizzard]]):
                 next_blizzard = Blizzard(wrapped_location, next_blizzard.direction)
             moved_blizzards.add(next_blizzard)
         return moved_blizzards
+
+    def blizzards(self, location: Coord, minute: int) -> set[Blizzard]:
+        return {blizzard for blizzard in self.get(minute) if blizzard.location == location}
 
 
 class Mountain:
@@ -149,19 +152,57 @@ class Mountain:
         return True
 
 
+def stringify_scene(mountain: Mountain, location: Coord, minute: int) -> str:
+    out = ""
+    for y in range(mountain.height):
+        for x in range(mountain.width):
+            pos = Coord(x, y)
+            if pos == location:
+                out += "E"
+            elif pos in mountain.border:
+                out += "#"
+            elif blizzards := mountain.blizzard_activity.blizzards(pos, minute):
+                if len(blizzards) == 1:
+                    out += blizzards.pop().direction.value
+                else:
+                    out += str(len(blizzards))
+            else:
+                out += "."
+
+        out += "\n"
+    return out
+
+
+def traverse(lines: list[str]) -> int:
+    mountain = Mountain(lines)
+    location = mountain.entrance
+    minutes = 0
+    directions = [Direction.DOWN, Direction.RIGHT, None, Direction.UP, Direction.LEFT]
+    queue = deque()
+    queue.append((location, minutes))
+    while location != mountain.exit:
+        print(stringify_scene(mountain, location, minutes))
+        for direction in directions:
+            next_location = location.move(direction) if direction else location
+            if mountain.can_move(next_location, minutes + 1):
+                break
+        minutes += 1
+        location = next_location
+    return minutes
+
+
 def run(filename: str) -> None:
     with open(filename, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f.readlines()]
     print(f"File: {filename}")
-    mountain = Mountain(lines)
-    # solution here
-    foo = mountain.can_move(Coord(1, 1), 5)
+    minutes = traverse(lines)
+    print(minutes)
     print()
 
 
 if __name__ == '__main__':
     start = time.time()
-    run("simple_example.txt")
-    # run("example.txt")
+    # run("simple_example.txt")
+    run("example.txt")
     # run("input.txt")
     print(f"Time to execute: {time.time() - start}")
