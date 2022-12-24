df3ada123113ba9f1abc7de133b1e8632ba81228
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-24 08:51:33 -0500

    Added blizzard activity

--- src/days/24/main.py
+++ src/days/24/main.py
@@ -56,15 +56,57 @@ class Blizzard:
         return Blizzard(self.location.move(self.direction), self.direction)
 
 
+class BlizzardActivity(dict[int, set[Blizzard]]):
+    width: int
+    height: int
+    border: set[Coord]
+
+    def __init__(self, width: int, height: int, border: set[Coord]) -> None:
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.border = border
+
+    def __getitem__(self, minute: int) -> set[Blizzard]:
+        if minute not in self:
+            self[minute] = self._move_blizzards(minute - 1)
+        return self[minute]
+
+    def _move_blizzards(self, minute: int) -> set[Blizzard]:
+        blizzards = self.get(minute)
+        moved_blizzards = set()
+        for blizzard in blizzards:
+            next_blizzard = blizzard.move()
+            if next_blizzard.location in self.border:
+                match next_blizzard.direction:
+                    case Direction.UP:
+                        wrapped_location = Coord(next_blizzard.location.x, self.height - 2)
+                    case Direction.RIGHT:
+                        wrapped_location = Coord(2, next_blizzard.location.y)
+                    case Direction.DOWN:
+                        wrapped_location = Coord(next_blizzard.location.x, 2)
+                    case Direction.LEFT:
+                        wrapped_location = Coord(self.width - 2, next_blizzard.location.y)
+                    case _:
+                        raise ValueError("Unrecognized direction")
+                next_blizzard = Blizzard(wrapped_location, next_blizzard.direction)
+            moved_blizzards.add(next_blizzard)
+        return moved_blizzards
+
+
 class Mountain:
     entrance: Coord
     exit: Coord
+    width: int
+    height: int
     border: set[Coord]
-    blizzards: set[Blizzard]
+    blizzards: BlizzardActivity
 
     def __init__(self, lines: list[str]) -> None:
         self.border = set()
-        self.blizzards = set()
+        self.height = len(lines)
+        self.width = len(lines[0])
+        blizzards = set()
         for y, row in enumerate(lines):
             for x, char in enumerate(row):
                 match char:
@@ -76,10 +118,12 @@ class Mountain:
                         if y == len(lines) - 1:
                             self.exit = Coord(x, y)
                     case "^" | "v" | "<" | ">":
-                        self.blizzards.add(Blizzard(Coord(x, y), Direction.from_char(char)))
+                        blizzards.add(Blizzard(Coord(x, y), Direction.from_char(char)))
 
         # Block the path above the entrance
         self.border.add(Coord(self.entrance.x, self.entrance.y - 1))
+        self.blizzard_activity = BlizzardActivity(self.width, self.height, self.border)
+        self.blizzard_activity[0] = blizzards
 
 
 def run(filename: str) -> None:
@@ -88,7 +132,7 @@ def run(filename: str) -> None:
     print(f"File: {filename}")
     mountain = Mountain(lines)
     # solution here
-
+    blizzards = mountain.move_blizzards()
     print()
 
 
