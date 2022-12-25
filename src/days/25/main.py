1ebe8b56950016dbe995a831fe8c26702e0e850c
Tim Fratangelo <tfratangelo@indigoag.com>
2022-12-25 10:27:46 -0500

    Almost have add working

--- src/days/25/main.py
+++ src/days/25/main.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import time
 from dataclasses import dataclass, field
-
+from functools import reduce
+from itertools import zip_longest, accumulate
+from operator import add
 
 SNAFU_VALUES = {
     "2": 2,
@@ -10,6 +12,14 @@ SNAFU_VALUES = {
     "0": 0,
     "-": -1,
     "=": -2
+}
+
+SNAFU_VALUES_REVERSED = {
+    2: "2",
+    1: "1",
+    0: "0",
+    -1: "-",
+    -2: "="
 }
 
 
@@ -28,6 +38,40 @@ class Snafu:
     def __repr__(self) -> str:
         return str(self)
 
+    """
+    21
+    1=
+    1=-
+    25 + -10 + -1
+    14
+    
+    2
+    2
+    1-
+    
+    """
+
+    def __add__(self, other: Snafu) -> Snafu:
+        sum_digits = []
+        carry = 0
+        for this, that in zip_longest(self.digits, other.digits):
+            if this is None:
+                digit = that
+            elif that is None:
+                digit = this
+            else:
+                digit = this + that
+            digit += carry
+            if digit > 2:
+                carry = 1
+                digit = -2 + (digit - 3)
+
+            sum_digits.append(digit)
+        if carry:
+            sum_digits.append(carry)
+        snafu_value = "".join([SNAFU_VALUES_REVERSED[digit] for digit in reversed(sum_digits)])
+        return Snafu(snafu_value)
+
     @property
     def base_10(self) -> int:
         out = 0
@@ -36,16 +80,15 @@ class Snafu:
         return out
 
 
-
-
 def run(filename: str) -> None:
     with open(filename, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f.readlines()]
     print(f"File: {filename}")
     snafus = [Snafu(line) for line in lines]
-    for snafu in snafus:
-        print(f"{snafu} = {snafu.base_10}")
-
+    # for snafu in snafus:
+    #     print(f"{snafu} = {snafu.base_10}")
+    total = reduce(add, snafus)
+    print(total, total.base_10)
     print()
 
 
